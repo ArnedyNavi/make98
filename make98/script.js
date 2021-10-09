a472a8146ce7283818bd55e631f9d9646e20de94
ArnedyNavi <alfonsus737@gmail.com>
2021-10-10 00:27:53 +0700

    Finish start bar

--- make98/script.js
+++ make98/script.js
@@ -24,6 +24,42 @@ $(document).ready(function () {
         }, 1000);
         document.getElementById('screen').style.display = 'block';
         document.getElementById('loadingScreen').style.display = 'none';
+
+        $("#menu").on("click", ".launch", function (event) {
+            console.log($(this).data("launch"));
+            var targetId = $(this).data("launch");
+            var title = $(this).data("title");
+            var imgUrl = $(this).data("icon");
+            var url = $(this).data("url");
+            if (!isWindowOpen(targetId)) {
+              createProgram(targetId, title, imgUrl, url);
+              $("#menu").hide();
+              $("#startbutton").removeClass("startbutton-on");
+            } else {
+              openWindow(targetId);
+              $("#menu").hide();
+              $("#startbutton").removeClass("startbutton-on");
+              console.log("program already exists... opening window");
+            }
+          });
+        
+          $(".item").each(function () {
+            if ($(this).data("icon")) {
+              var icon = $(this).data("icon");
+              $(this).css("background-image", "url(" + icon + ")");
+            } else {
+              console.log("no icon supplied");
+            }
+          });
+        
+          $(".dropdown-item").each(function () {
+            if ($(this).data("icon")) {
+              var icon = $(this).data("icon");
+              $(this).css("background-image", "url(" + icon + ")");
+            } else {
+              console.log("no icon supplied");
+            }
+          });
     })
 });
 
@@ -34,11 +70,14 @@ function modifyStart() {
         element.classList.add("startbutton-unclicked");
         element.classList.remove("startbutton-clicked");
         startStatus = false;
+        document.getElementById('menu').style.display = 'none';
     }
     else {
         var element = document.getElementById("startButton");
         element.classList.add("startbutton-clicked");
         element.classList.remove("startbutton-unclicked");
         startStatus = true;
+        document.getElementById('menu').style.display = 'block';
     }
 }
+
